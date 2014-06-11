cf27b94c61c393f387dcfe35d293826d686ab47f
glonsberry <gardnerlonsberry@gmail.com>
2014-06-11 16:35:32 -0400

    Implement show, edit, update, destroy actions

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,18 +6,28 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = book.find(params[:id]
   end
 
   def create
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    edit_book = Book.find(params[:id])
+    edit_book.update(book_params)
+    redirect_to "/books/#{book.id}"
   end
 
   def destroy
   end
 
+
+private
+
+def book_params
+  params.require(:book).permit(:title, :author)
 end
